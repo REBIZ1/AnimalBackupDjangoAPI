6ba711e2f2cb6916c20f49f7d2aff1a6bd2b1121
Arthur <arthur155188@gmail.com>
2025-11-11 19:15:35 +0500

    Добавлен класс Dogs для работы с изображениями котов - Метод get_cat_with_text получает картинку кота с текстом и возвращает словарь с данными

---
+++ animals/services/cats.py
@@ -0,0 +1,34 @@
+import requests
+
+
+class Cats:
+    """Класс для работы с API cataas.com"""
+    base_url = 'https://cataas.com'
+
+    @staticmethod
+    def get_cat_with_text(text: str):
+        """
+        Получает картинку кота с текстом и возвращает словарь с данными.
+        входные данные:
+        - text: Текст для картинки
+        Выходные данные:
+        - Словарь:
+            - filename: имя файла
+            - size_bytes: размер картинки
+            - image: байтовое содержимое картинки
+        """
+        try:
+            response = requests.get(f'{Cats.base_url}/cat/says/{text}', timeout=10)
+            response.raise_for_status()
+            image = response.content
+            result = {
+                'filename': f"{text}.jpg",
+                'size_bytes': len(image),
+                'image': image
+            }
+            return result
+        except requests.exceptions.RequestException as e:
+            print('Ошибка при получении картинки:', e)
+            return None
+
+
