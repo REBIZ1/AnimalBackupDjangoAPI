a2fb7debfafa111794c4f4726a161be016f99bf2
Arthur <arthur155188@gmail.com>
2025-11-19 20:17:29 +0500

    Настроил urls

--- AnimalBackupDjangoAPI/urls.py
+++ AnimalBackupDjangoAPI/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from animals.views import index, save_token, cats_page, get_cat_image, upload_cat_to_disk, dogs_page, get_dog_image, upload_dog_to_disk
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", index, name="index"),
+    path("save-token/", save_token, name="save_token"),
+    path("cats/", cats_page, name="cats_page"),
+    path("cats/get/", get_cat_image, name="get_cat_image"),
+    path("cats/upload/", upload_cat_to_disk, name="upload_cat_to_disk"),
+    path("dogs/", dogs_page, name="dogs_page"),
+    path("dogs/get/", get_dog_image, name="get_dog_image"),
+    path("dogs/upload/", upload_dog_to_disk, name="upload_dog_to_disk"),
 ]
